bf3efc0b6f75ed6dee91d3c6c801b5d5de0bf6b6
leocvidal <leo.vidal@au1.ibm.com>
2024-08-08 13:53:23 +1000

    Many changes

--- scripts/deploy_consumer_qm.sh
+++ scripts/deploy_consumer_qm.sh
@@ -6,6 +6,11 @@ export KEYP12=ams.p12
 export PASSWORD=password
 export STASH=ams.sth
 
+rm ${KEY}
+rm ${CERT}
+rm ${KEYP12}
+rm ${STASH}
+
 # Create a private key and certificate in PEM format, for the server to use
 echo "#### Create a private key and certificate in PEM format, for the server to use"
 openssl req \
@@ -31,11 +36,6 @@ oc cp ./${KEYP12} mq-temp-ibm-mq-0:/tmp/jenkins_pipeline -n jenkins -c qmgr
 echo "#### Creating kdb key store, for the server to use"
 oc exec mq-temp-ibm-mq-0 -n jenkins -- bash -c "runmqakm -keydb -create -db /tmp/jenkins_pipeline/${KEYDB} -pw ${PASSWORD} -stash"
 
-#copy .kdb to /conf directory
-# copying too early??
-# oc cp mq-temp-ibm-mq-0:/tmp/jenkins_pipeline/${KEYDB} ./conf/${KEYDB} -n jenkins -c qmgr
-# oc cp mq-temp-ibm-mq-0:/tmp/jenkins_pipeline/${STASH} ./conf/${STASH} -n jenkins -c qmgr
-
 # Add the key and certificate to a kdb key store, for the server to use
 echo "#### Adding certs and keys to kdb key store, for the server to use"
 #runmqckm -cert -add -db ${KEYDB} -file ${CERT} -stashed
@@ -48,7 +48,7 @@ oc cp mq-temp-ibm-mq-0:/tmp/jenkins_pipeline/${KEYDB} ./conf/${KEYDB} -n jenkins
 oc cp mq-temp-ibm-mq-0:/tmp/jenkins_pipeline/${STASH} ./conf/${STASH} -n jenkins -c qmgr
 
 #Inititate execution
-oc project westpac-demo
+oc project consumer-app
 set +e
 # Remove the runnning queue manager instance (if any)
 
@@ -56,9 +56,9 @@ set +e
 oc delete qmgr mq-ams
 
 # Delete the route object and secret for the QueueManager keystore (if any), and the mqsc configMap
-oc delete route mq-amsroute
+oc delete route mq-amsroute-consumer-app
 oc delete secret mqamskey
-oc delete configMap ams1-mqsc
+oc delete configMap ams1-mqsc-remote
 oc delete secret kdb-secret
 oc delete secret ams-conf
 set -e
@@ -68,8 +68,7 @@ oc create secret tls mqamskey --cert=./tls/tls.crt --key=./tls/tls.key
 oc create secret generic kdb-secret --from-file=ams.kdb=./conf/ams.kdb --from-file=ams.sth=./conf/ams.sth 
 oc create secret generic ams-conf --from-file=keystore.conf=./conf/keystore.conf
 
-oc create -f mqsc/mqsc.yaml
+oc create -f mqsc/mqsc_remote.yaml
 
 set -e
-# oc apply -f mqeploy.yaml
-oc apply -f mqNoinitC.yaml
+oc apply -f mqNoinitC_remote.yaml
