afb6c90efe7acc072d968fcc44f6ffc73ea9f9ef
leocvidal <leo.vidal@au1.ibm.com>
2024-08-13 11:45:04 +1000

    updates from TLS3 to TLS5

--- scripts/deploy_consumer_qm.sh
+++ scripts/deploy_consumer_qm.sh
@@ -63,10 +63,10 @@ oc delete secret kdb-secret
 oc delete secret ams-conf
 set -e
 # Create the route and the keystore secret and mqsc configMap
-oc apply -f mq-amsRoute.yaml
+oc apply -f mq-amsRoute.consumer-app.yaml
 oc create secret tls mqamskey --cert=./tls/tls.crt --key=./tls/tls.key
 oc create secret generic kdb-secret --from-file=ams.kdb=./conf/ams.kdb --from-file=ams.sth=./conf/ams.sth 
-oc create secret generic ams-conf --from-file=keystore.conf=./conf/keystore.conf
+oc create secret generic ams-conf --from-file=keystore.remote.conf=./conf/keystore.remote.conf
 
 oc create -f mqsc/mqsc_remote.yaml
  
